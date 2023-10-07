86972dd1d57991b8bd663f4a58b8c33c4d698913
Dorian <dorianhl5229@gmail.com>
2023-10-06 18:09:38 -0600

    Refresh

--- 01-gato/src/App.jsx
+++ 01-gato/src/App.jsx
@@ -1,26 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import confetti from 'canvas-confetti'
 import {Square} from './components/Square.jsx'
 import { TURNS } from './constants.jsx'
 import {checkWinnerFrom, checkEndGame} from './logic/board.jsx'
 import { WinnerModal } from './components/WinnerModal.jsx'
+import { resetGameStorage, saveGameToStorage } from './logic/storage'
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    console.log('inicializar estado del board')
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+})
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X /* EL OPERADOR ?? MIRA SI ES NULL O UNDEFINED */
+  })
   // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null) 
-
+  
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-  }  
 
-
+    resetGameStorage()
+  } 
 
   const updateBoard = (index ) => {
     // no actualizamos esta posicion
@@ -33,6 +41,10 @@ function App() {
     // cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // guardar aqui partida
+    saveGameToStorage( {
+      board: newBoard, 
+      turn: newTurn} )
     // revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
@@ -42,8 +54,6 @@ function App() {
       setWinner(false) //Empate
     }
   }
-
-
 
   return (
     <main className='board'>
